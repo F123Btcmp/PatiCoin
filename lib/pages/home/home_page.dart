b98188e50da3f2775e88c765b7757a2e4dd08373
ibrahim ethem <21060671@stu.omu.edu.tr>
2023-06-13 00:16:44 +0300

    appBarProfile tasarımı tamamlandı.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
-  const HomePage({super.key});
+import '../../classes/app_bar_profile.dart';
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+    var size = MediaQuery.of(context).size ;
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar:  appBarCustom(title: "Profil"),
+        body: SingleChildScrollView(
+
+        )
       ),
     );
   }
