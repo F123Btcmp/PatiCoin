232cced2c8e7fbf742b5e5908f4b57b32e5de915
ibrahim ethem <21060671@stu.omu.edu.tr>
2023-07-18 12:08:23 +0300

    dikeylik

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'App/material_root.dart';
 
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,  // Sadece Dikeyde çalışması için .
+  ]);
   await Firebase.initializeApp();
   runApp(const ProviderScope(
           child: MyApp()
